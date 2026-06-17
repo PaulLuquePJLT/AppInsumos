79daa0806259e435ad947f775e8cccde9792415e
PaulLuquePJLT <paul911000@gmail.com>
2026-06-17 16:16:26 -0500

    Add functions for managing product movements

--- pages/08_Movimientos.py
+++ pages/08_Movimientos.py
@@ -1,6 +1,105 @@
-import streamlit as st
-from src.queries import get_movimientos
+from sqlalchemy import text
 
-st.title('📜 Movimientos')
-movimientos = get_movimientos()
-st.dataframe(movimientos)
+from src.db import get_engine
+
+
+def registrar_entrada(
+    id_producto,
+    id_ubicacion_destino,
+    cantidad,
+    id_usuario,
+    referencia=None,
+    observacion=None,
+    lote=None,
+):
+    query = text("""
+        EXEC sp_registrar_entrada
+            @id_producto = :id_producto,
+            @id_ubicacion_destino = :id_ubicacion_destino,
+            @cantidad = :cantidad,
+            @id_usuario = :id_usuario,
+            @referencia = :referencia,
+            @observacion = :observacion,
+            @lote = :lote
+    """)
+    params = {
+        "id_producto": id_producto,
+        "id_ubicacion_destino": id_ubicacion_destino,
+        "cantidad": cantidad,
+        "id_usuario": id_usuario,
+        "referencia": referencia,
+        "observacion": observacion,
+        "lote": lote,
+    }
+    with get_engine().begin() as conn:
+        conn.execute(query, params)
+
+
+def registrar_salida_cuenta(
+    id_producto,
+    id_ubicacion_origen,
+    id_cuenta,
+    cantidad,
+    id_usuario,
+    referencia=None,
+    observacion=None,
+    lote=None,
+):
+    query = text("""
+        EXEC sp_registrar_salida_cuenta
+            @id_producto = :id_producto,
+            @id_ubicacion_origen = :id_ubicacion_origen,
+            @id_cuenta = :id_cuenta,
+            @cantidad = :cantidad,
+            @id_usuario = :id_usuario,
+            @referencia = :referencia,
+            @observacion = :observacion,
+            @lote = :lote
+    """)
+    params = {
+        "id_producto": id_producto,
+        "id_ubicacion_origen": id_ubicacion_origen,
+        "id_cuenta": id_cuenta,
+        "cantidad": cantidad,
+        "id_usuario": id_usuario,
+        "referencia": referencia,
+        "observacion": observacion,
+        "lote": lote,
+    }
+    with get_engine().begin() as conn:
+        conn.execute(query, params)
+
+
+def registrar_transferencia(
+    id_producto,
+    id_ubicacion_origen,
+    id_ubicacion_destino,
+    cantidad,
+    id_usuario,
+    referencia=None,
+    observacion=None,
+    lote=None,
+):
+    query = text("""
+        EXEC sp_registrar_transferencia
+            @id_producto = :id_producto,
+            @id_ubicacion_origen = :id_ubicacion_origen,
+            @id_ubicacion_destino = :id_ubicacion_destino,
+            @cantidad = :cantidad,
+            @id_usuario = :id_usuario,
+            @referencia = :referencia,
+            @observacion = :observacion,
+            @lote = :lote
+    """)
+    params = {
+        "id_producto": id_producto,
+        "id_ubicacion_origen": id_ubicacion_origen,
+        "id_ubicacion_destino": id_ubicacion_destino,
+        "cantidad": cantidad,
+        "id_usuario": id_usuario,
+        "referencia": referencia,
+        "observacion": observacion,
+        "lote": lote,
+    }
+    with get_engine().begin() as conn:
+        conn.execute(query, params)
