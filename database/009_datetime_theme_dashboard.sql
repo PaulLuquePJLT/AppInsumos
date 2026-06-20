7c05ac89f26d7a0a2f6bc78549a9f04c1d3baa1d
PaulLuquePJLT <paul911000@gmail.com>
2026-06-20 13:06:45 -0500

    Refactor date/time function and default value for movimientos
    Updated the function for local date/time in Bogotá-Lima and adjusted the default value for 'fecha_movimiento' in the 'movimientos' table.

--- database/009_datetime_theme_dashboard.sql
+++ database/009_datetime_theme_dashboard.sql
@@ -1,81 +1,69 @@
 SET NOCOUNT ON;
 
-/* ================================================================
-   009 - Fecha/hora local Bogota-Lima, vista dashboard y UI
-   ================================================================ */
+/* ==========================================================
+   Corrección función fecha/hora Bogotá - Lima - Perú UTC-5
+   ========================================================== */
 
-/* 1) Funcion central para fecha/hora local Bogota/Lima/Peru.
-      Bogota y Lima usan UTC-05:00. Se usa SYSUTCDATETIME para evitar
-      depender de la zona horaria del servidor de Azure SQL. */
-CREATE OR ALTER FUNCTION dbo.fn_now_bogota_lima()
+IF OBJECT_ID('dbo.fn_now_bogota_lima', 'FN') IS NOT NULL
+BEGIN
+    DROP FUNCTION dbo.fn_now_bogota_lima;
+END;
+
+EXEC(N'
+CREATE FUNCTION dbo.fn_now_bogota_lima()
 RETURNS DATETIME2(0)
 AS
 BEGIN
-    RETURN CAST(DATEADD(HOUR, -5, SYSUTCDATETIME()) AS DATETIME2(0));
-END;
-GO
+    RETURN CAST(SWITCHOFFSET(SYSDATETIMEOFFSET(), ''-05:00'') AS DATETIME2(0));
+END
+');
 
-/* 2) Default de movimientos.fecha_movimiento con hora local de ejecucion. */
+/* ==========================================================
+   Actualizar valores NULL si existieran
+   ========================================================== */
+
 IF OBJECT_ID('dbo.movimientos', 'U') IS NOT NULL
-   AND COL_LENGTH('dbo.movimientos', 'fecha_movimiento') IS NOT NULL
 BEGIN
-    DECLARE @constraint_name SYSNAME;
+    UPDATE dbo.movimientos
+    SET fecha_movimiento = dbo.fn_now_bogota_lima()
+    WHERE fecha_movimiento IS NULL;
+END;
 
-    SELECT @constraint_name = dc.name
-    FROM sys.default_constraints dc
-    INNER JOIN sys.columns c
-        ON c.default_object_id = dc.object_id
-    WHERE dc.parent_object_id = OBJECT_ID('dbo.movimientos')
-      AND c.name = 'fecha_movimiento';
+/* ==========================================================
+   Eliminar default anterior de movimientos.fecha_movimiento
+   ========================================================== */
 
-    IF @constraint_name IS NOT NULL
-    BEGIN
-        DECLARE @sql NVARCHAR(MAX) = N'ALTER TABLE dbo.movimientos DROP CONSTRAINT ' + QUOTENAME(@constraint_name) + N';';
-        EXEC sp_executesql @sql;
-    END;
+DECLARE @constraint_name NVARCHAR(200);
+DECLARE @sql NVARCHAR(MAX);
 
+SELECT @constraint_name = dc.name
+FROM sys.default_constraints dc
+INNER JOIN sys.columns c
+    ON c.default_object_id = dc.object_id
+INNER JOIN sys.tables t
+    ON t.object_id = c.object_id
+INNER JOIN sys.schemas s
+    ON s.schema_id = t.schema_id
+WHERE s.name = 'dbo'
+  AND t.name = 'movimientos'
+  AND c.name = 'fecha_movimiento';
+
+IF @constraint_name IS NOT NULL
+BEGIN
+    SET @sql = N'ALTER TABLE dbo.movimientos DROP CONSTRAINT ' + QUOTENAME(@constraint_name);
+    EXEC sp_executesql @sql;
+END;
+
+/* ==========================================================
+   Crear nuevo default con hora Bogotá/Lima
+   ========================================================== */
+
+IF OBJECT_ID('dbo.movimientos', 'U') IS NOT NULL
+BEGIN
     ALTER TABLE dbo.movimientos
     ADD CONSTRAINT DF_movimientos_fecha_movimiento_bogota_lima
-    DEFAULT dbo.fn_now_bogota_lima() FOR fecha_movimiento;
+    DEFAULT dbo.fn_now_bogota_lima()
+    FOR fecha_movimiento;
 END;
-GO
 
-/* 3) Vista de movimientos enriquecida para dashboard. */
-CREATE OR ALTER VIEW dbo.vw_movimientos AS
-SELECT
-    m.id_movimiento,
-    m.tipo_movimiento,
-    m.fecha_movimiento,
-    ISNULL(c.codigo_cuenta, '') AS codigo_cuenta,
-    ISNULL(c.nombre_cuenta, '') AS nombre_cuenta,
-    ISNULL(pr.ruc, '') AS ruc_proveedor,
-    ISNULL(pr.razon_social, '') AS razon_social_proveedor,
-    p.sku,
-    p.nombre_producto,
-    ISNULL(um.codigo_unidad, '') AS codigo_unidad,
-    ISNULL(um.nombre_unidad, '') AS nombre_unidad,
-    ub_origen.codigo_ubicacion AS ubicacion_origen,
-    ub_destino.codigo_ubicacion AS ubicacion_destino,
-    CAST(ISNULL(md.cantidad, 0) AS DECIMAL(18,2)) AS cantidad,
-    md.lote,
-    m.referencia,
-    m.observacion,
-    md.observacion AS texto_item,
-    m.id_usuario,
-    ISNULL(u.usuario_login, '') AS usuario_login,
-    LTRIM(RTRIM(ISNULL(u.nombres, '') + ' ' + ISNULL(u.apellidos, ''))) AS usuario_nombre,
-    m.estado
-FROM dbo.movimientos m
-LEFT JOIN dbo.movimiento_detalle md ON md.id_movimiento = m.id_movimiento
-LEFT JOIN dbo.productos p ON p.id_producto = md.id_producto
-LEFT JOIN dbo.unidades_medida um ON um.id_unidad = p.id_unidad
-LEFT JOIN dbo.ubicaciones ub_origen ON ub_origen.id_ubicacion = md.id_ubicacion_origen
-LEFT JOIN dbo.ubicaciones ub_destino ON ub_destino.id_ubicacion = md.id_ubicacion_destino
-LEFT JOIN dbo.cuentas_logisticas c ON c.id_cuenta = m.id_cuenta
-LEFT JOIN dbo.proveedores pr ON pr.id_proveedor = m.id_proveedor
-LEFT JOIN dbo.usuarios u ON u.id_usuario = m.id_usuario;
-GO
-
-SELECT
-    'OK - 009 aplicado. Fecha movimiento ahora usa dbo.fn_now_bogota_lima().' AS resultado,
-    dbo.fn_now_bogota_lima() AS fecha_hora_bogota_lima;
+SELECT dbo.fn_now_bogota_lima() AS fecha_hora_bogota_lima;
